4a21aeda98f1f3bc347d48e1d08b58d1b4ac7aea
Simon Lynch <simon.lynch@hashicorp.com>
2024-10-11 13:00:16 +1100

    Refactor Ansible manifest: Remove unused import and commented code in tfe.tf

---
+++ tfe.tf
@@ -0,0 +1,21 @@
+import {
+  to = helm_release.tfe
+  id = "terraform-enterprise"
+}
+
+# deploy tfe using helm chart
+resource "helm_release" "tfe" {
+  name       = "terraform-enterprise"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "terraform-enterprise"
+  version    = "1.3.2"
+  create_namespace = false
+  namespace = "tfe"
+  wait = false
+  force_update = true
+
+  values = [
+    local.tfe_helm_values
+  ]
+
+}
