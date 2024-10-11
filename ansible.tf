428208ce179a4f1c2c6e55a1ff5a59cbf6beefe8
Simon Lynch <simon.lynch@hashicorp.com>
2024-10-11 11:14:27 +1100

    import ansible

---
+++ ansible.tf
@@ -0,0 +1,37 @@
+locals {
+# Ansible Automation Platform
+aap_controller = file("${path.module}/manifests/ansible/aap-controller.yaml")
+aap_hub= file("${path.module}/manifests/ansible/aap-hub.yaml")
+aap_eda = file("${path.module}/manifests/ansible/aap-eda.yaml")
+}
+
+
+import {
+  to = kubernetes_manifest.aap-controller
+  id = "apiVersion=automationcontroller.ansible.com/v1beta1,kind=AutomationController,namespace=aap,name=controller"
+}
+
+# Ansible Controller resource
+resource "kubernetes_manifest" "aap-controller" {
+  manifest = provider::kubernetes::manifest_decode(local.aap_controller)
+}
+
+import {
+  to = kubernetes_manifest.aap-eda
+  id = "apiVersion=eda.ansible.com/v1alpha1,kind=EDA,namespace=aap,name=eda"
+}
+
+# Ansible EDA resource
+resource "kubernetes_manifest" "aap-eda" {
+  manifest = provider::kubernetes::manifest_decode(local.aap_eda)
+}
+
+import {
+  to = kubernetes_manifest.aap-hub
+  id = "apiVersion=automationhub.ansible.com/v1beta1,kind=AutomationHub,namespace=aap,name=hub"
+}
+
+# Ansible Automation Hub
+resource "kubernetes_manifest" "aap-hub" {
+  manifest = provider::kubernetes::manifest_decode(local.aap_hub)
+}
