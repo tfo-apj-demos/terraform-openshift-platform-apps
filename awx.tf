e454db90d60cdd17b2ba40d0cb51d078c5e7c47e
Aaron Evans <aarone@outlook.com.au>
2024-10-24 09:41:44 +1100

    Refactor AWX manifest: Rename ServiceAccount file and add ClusterRoleBinding resource

--- awx.tf
+++ awx.tf
@@ -1,7 +1,8 @@
 locals {
 # Ansible Automation Platform
 awx = file("${path.module}/manifests/awx/awx.yaml")
-awx-sa = file("${path.module}/manifests/awx/awx-sa.yaml")
+vault-auth-sa = file("${path.module}/manifests/awx/awx-sa.yaml")
+awx-clusterrolebinding = file("${path.module}/manifests/awx/awx-clusterrolebinding.yaml")
 }
 
 resource "kubernetes_secret_v1" "awx-admin-password" {
@@ -25,7 +26,15 @@ resource "kubernetes_manifest" "awx" {
 
 resource "kubernetes_manifest" "awx-sa" {
   depends_on = [ kubernetes_manifest.awx ]
-  manifest = provider::kubernetes::manifest_decode(local.awx-sa)
+  manifest = provider::kubernetes::manifest_decode(local.vault-auth-sa)
+  field_manager {
+    force_conflicts = true
+  }
+}
+
+resource "kubernetes_manifest" "awx-clusterrolebinding" {
+  depends_on = [ kubernetes_manifest.awx-sa ]
+  manifest = provider::kubernetes::manifest_decode(local.awx-clusterrolebinding)
   field_manager {
     force_conflicts = true
   }
