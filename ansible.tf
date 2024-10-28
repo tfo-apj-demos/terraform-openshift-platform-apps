ea1c87eba6b8491ecf0c17fa3bd0c6d0d6f37985
Simon Lynch <simon.lynch@hashicorp.com>
2024-10-29 09:40:46 +1100

    Refactor ansible.tf: Remove commented out AAP resources

--- ansible.tf
+++ ansible.tf
@@ -1,36 +1,36 @@
 locals {
   # Ansible Automation Platform
-  aap_controller = file("${path.module}/manifests/ansible/aap-controller.yaml")
-  aap_hub= file("${path.module}/manifests/ansible/aap-hub.yaml")
-  aap_eda = file("${path.module}/manifests/ansible/aap-eda.yaml")
+  # aap_controller = file("${path.module}/manifests/ansible/aap-controller.yaml")
+  # aap_hub= file("${path.module}/manifests/ansible/aap-hub.yaml")
+  # aap_eda = file("${path.module}/manifests/ansible/aap-eda.yaml")
   aap-vault-auth-sa = file("${path.module}/manifests/ansible/aap-vault-auth-sa.yaml")
 }
 
 
-import {
-  to = kubernetes_manifest.aap-controller
-  id = "apiVersion=automationcontroller.ansible.com/v1beta1,kind=AutomationController,namespace=aap,name=controller"
-}
+# import {
+#   to = kubernetes_manifest.aap-controller
+#   id = "apiVersion=automationcontroller.ansible.com/v1beta1,kind=AutomationController,namespace=aap,name=controller"
+# }
 
-# Ansible Controller resource
-resource "kubernetes_manifest" "aap-controller" {
-  manifest = provider::kubernetes::manifest_decode(local.aap_controller)
-  field_manager {
-    force_conflicts = true
-  }
-}
-
-
-# Ansible EDA resource
-resource "kubernetes_manifest" "aap-eda" {
-  manifest = provider::kubernetes::manifest_decode(local.aap_eda)
-}
+# # Ansible Controller resource
+# resource "kubernetes_manifest" "aap-controller" {
+#   manifest = provider::kubernetes::manifest_decode(local.aap_controller)
+#   field_manager {
+#     force_conflicts = true
+#   }
+# }
 
 
-# Ansible Automation Hub
-resource "kubernetes_manifest" "aap-hub" {
-  manifest = provider::kubernetes::manifest_decode(local.aap_hub)
-}
+# # Ansible EDA resource
+# resource "kubernetes_manifest" "aap-eda" {
+#   manifest = provider::kubernetes::manifest_decode(local.aap_eda)
+# }
+
+
+# # Ansible Automation Hub
+# resource "kubernetes_manifest" "aap-hub" {
+#   manifest = provider::kubernetes::manifest_decode(local.aap_hub)
+# }
 
 resource "kubernetes_manifest" "aap-sa" {
   manifest = provider::kubernetes::manifest_decode(local.aap-vault-auth-sa)
