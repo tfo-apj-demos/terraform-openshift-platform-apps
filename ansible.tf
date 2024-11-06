3edd66b1d8bcf46d8e14e1cb9f54362841bf3e9a
Simon Lynch <simon.lynch@hashicorp.com>
2024-11-06 14:37:17 +1100

    Refactor ansible.tf: Add new Vault and PKI configurations

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,9 @@
 locals {
   aap-vault-auth-sa = file("${path.module}/manifests/ansible/aap-vault-auth-sa.yaml")
   aap-platform = file("${path.module}/manifests/ansible/aap-platform.yaml")
+  aap-pki = file("${path.module}/manifests/ansible/crd-pki-aapcert.yaml")
+  aap-vaultauth = file("${path.module}/manifests/ansible/crd-vault-auth.yaml")
+  aap-vaultconnection = file("${path.module}/manifests/ansible/crd-vault-connection.yaml")
 }
 
 
@@ -14,4 +17,18 @@ resource "kubernetes_manifest" "aap-controller" {
 
 resource "kubernetes_manifest" "aap-sa" {
   manifest = provider::kubernetes::manifest_decode(local.aap-vault-auth-sa)
+}
+
+
+
+resource "kubernetes_manifest" "aap-pki" {
+  manifest = provider::kubernetes::manifest_decode(local.aap-pki)
+}
+
+resource "kubernetes_manifest" "aap-pki" {
+  manifest = provider::kubernetes::manifest_decode(local.aap-vaultauth)
+}
+
+resource "kubernetes_manifest" "aap-pki" {
+  manifest = provider::kubernetes::manifest_decode(local.aap-vaultconnection)
 }
