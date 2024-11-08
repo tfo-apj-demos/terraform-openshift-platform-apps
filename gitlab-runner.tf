426ee632813a8f416a9c007a75002918052806c4
Aaron Evans <aarone@outlook.com.au>
2024-11-08 21:09:25 +1100

    Add GitLab runner Kubernetes secret and manifest resource

---
+++ gitlab-runner.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_secret" "gitlab_runner_token" {
+  metadata {
+    name      = "gitlab-runner-secret"
+    namespace = "gitlab-runner" 
+  }
+
+  type = "Opaque"
+
+  data = {
+    "runner-token" = base64encode(var.gitlab_runner_token)
+  }
+}
+
+locals {
+  gitlab_runner_manifest = file("${path.module}/manifests/gitlab-runner/crd-runner.yaml")
+}
+
+resource "kubernetes_manifest" "gitlab_runner" {
+  depends_on = [kubernetes_secret.gitlab_runner_token]
+  manifest   = provider::kubernetes::manifest_decode(local.gitlab_runner_manifest)
+}
