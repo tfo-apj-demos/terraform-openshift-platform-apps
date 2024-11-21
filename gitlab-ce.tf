1ab9a64d8ccc36e698ba46ab1549d1f87de6bce8
Simon Lynch <simon.lynch@hashicorp.com>
2024-11-22 09:38:46 +1100

    Add GitLab CE manifests and Vault resources for deployment

--- gitlab-ce.tf
+++ gitlab-ce.tf
@@ -1,6 +1,26 @@
 locals {
-  gitlab_deployment = file("${path.module}/manifests/gitlab-cde/gitlab-deployment.yaml")
-  gitlab_service = file("${path.module}/manifests/gitlab-cde/gitlab-service.yaml")
-  gitlab_pvc_logs = file("${path.module}/manifests/gitlab-cde/gitlab-pvc-logs.yaml")
-  gitlab_route = file("${path.module}/manifests/gitlab-cde/gitlab-route.yaml")
+  gitlab_deployment = file("${path.module}/manifests/gitlab-ce/gitlab-deployment.yaml")
+  gitlab_service = file("${path.module}/manifests/gitlab-ce/gitlab-service.yaml")
+  gitlab_pvc_logs = file("${path.module}/manifests/gitlab-ce/gitlab-pvc-logs.yaml")
+  gitlab_pvc_data = file("${path.module}/manifests/gitlab-ce/gitlab-pvc-data.yaml")
+  gitlab_pvc_config = file("${path.module}/manifests/gitlab-ce/gitlab-pvc-config.yaml")
+  gitlab_route = file("${path.module}/manifests/gitlab-ce/gitlab-route.yaml")
+
+  #Gitlab Vault Resources for certificate
+  gitlab_crd_vaultauth = file("${path.module}/manifests/gitlab-ce/crd-vault-auth.yaml")
+  gitlab_crd_vaultconnection = file("${path.module}/manifests/gitlab-ce/crd-vault-connection.yaml")
+  gitlab_pki-cert = file("${path.module}/manifests/gitlab-ce/crd-pki-gitlabcert.yaml")
+
+}
+
+resource "kubernetes_namespace" "gitlab" {
+  metadata {
+    name = "gitlab"
+  }
+  
+}
+
+resource "kubernetes_manifest" "gitlab_deployment" {
+  depends_on = [kubernetes_namespace.gitlab]
+  manifest   = provider::kubernetes::manifest_decode(local.gitlab_deployment)
 }
