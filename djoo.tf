0d1d8888e04e8734c9e042929759cefc0a844791
Aaron Evans <aarone@outlook.com.au>
2024-11-26 14:41:08 +1100

    Add Kubernetes namespace resource for djoo with lifecycle management

---
+++ djoo.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_namespace" "djoo-namespace" {
+  metadata {
+    name = "djoo"
+  }  
+  lifecycle{
+    ignore_changes = [
+        metadata.0.annotations["openshift.io/sa.scc.mcs"],
+        metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
+        metadata.0.annotations["openshift.io/sa.scc.uid-range"]
+    ]
+  }
+}
