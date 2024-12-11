8e710c012365e59bf7609d959ad6bdcd28115abc
Simon Lynch <simon.lynch@hashicorp.com>
2024-12-11 21:03:41 +1100

    Rename Kubernetes manifest resource for Backstage operator group

--- rhdh_backstage.tf
+++ rhdh_backstage.tf
@@ -1,11 +1,9 @@
 locals {
   rhdh_backstage = file("${path.module}/manifests/backstage/crd-rhdh-backstage.yaml")
-
 }
 
 
-
-resource "kubernetes_manifest" "rhdh_operator_group" {
+resource "kubernetes_manifest" "rhdh_backstage" {
   manifest = provider::kubernetes::manifest_decode(local.rhdh_backstage)
 }
 
