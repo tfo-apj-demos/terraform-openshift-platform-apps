89298d7e1f464061008b5e5b69fc5b5f700aa433
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-11 16:03:25 +1100

    Rename Kubernetes manifest resources for IBM Db2 and Keycloak for clarity

--- ibm-db2.tf
+++ ibm-db2.tf
@@ -2,6 +2,6 @@ locals {
   db2_instance = file("${path.module}/manifests/ibm-db2/db2-instance.yaml")
 }
 
-resource "kubernetes_manifest" "instance" {
+resource "kubernetes_manifest" "db2" {
     manifest = provider::kubernetes::manifest_decode(local.db2_instance)
 }
--- keycloak.tf
+++ keycloak.tf
@@ -2,6 +2,6 @@ locals {
   keycloak_instance = file("${path.module}/manifests/keycloak/keycloak-instance.yaml")
 }
 
-resource "kubernetes_manifest" "instance" {
+resource "kubernetes_manifest" "keycloak" {
     manifest = provider::kubernetes::manifest_decode(local.keycloak_instance)
 }
