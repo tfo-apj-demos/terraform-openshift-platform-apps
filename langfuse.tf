a88e00e1fadb80159706880f7fb1883c1d1aa09d
Aaron Evans <aarone@outlook.com.au>
2026-01-25 12:24:17 +1100

    Refactor Langfuse and Sentry configurations: comment out existing resources and add placeholders for future integration with Vault Secrets Operator

--- langfuse.tf
+++ langfuse.tf
@@ -1,141 +1,141 @@
-# Langfuse - LLM Observability Platform
-# Deployed via Helm chart with external S3 storage (Ceph RGW)
+# # Langfuse - LLM Observability Platform
+# # Deployed via Helm chart with external S3 storage (Ceph RGW)
 
-locals {
-  langfuse_route            = file("${path.module}/manifests/langfuse/langfuse-route.yaml")
-  langfuse_vault_connection = file("${path.module}/manifests/langfuse/vault-connection.yaml")
-  langfuse_vault_auth       = file("${path.module}/manifests/langfuse/vault-auth.yaml")
-  langfuse_vault_auth_sa    = file("${path.module}/manifests/langfuse/vault-auth-sa.yaml")
-  langfuse_pki_cert         = file("${path.module}/manifests/langfuse/crd-pki-langfusecert.yaml")
+# locals {
+#   langfuse_route            = file("${path.module}/manifests/langfuse/langfuse-route.yaml")
+#   langfuse_vault_connection = file("${path.module}/manifests/langfuse/vault-connection.yaml")
+#   langfuse_vault_auth       = file("${path.module}/manifests/langfuse/vault-auth.yaml")
+#   langfuse_vault_auth_sa    = file("${path.module}/manifests/langfuse/vault-auth-sa.yaml")
+#   langfuse_pki_cert         = file("${path.module}/manifests/langfuse/crd-pki-langfusecert.yaml")
 
-  langfuse_helm_values = templatefile("${path.module}/templates/langfuse_helm_values.yaml.tpl", {
-    hostname            = "langfuse.apps.openshift-01.hashicorp.local"
-    storage_class       = "ocs-storagecluster-cephfs"
-    s3_endpoint         = "http://rook-ceph-rgw-ocs-storagecluster-cephobjectstore.openshift-storage.svc"
-    s3_bucket           = data.kubernetes_resource.langfuse_s3.object.spec.bucketName
-    s3_access_key       = data.kubernetes_secret.langfuse_s3.data.AWS_ACCESS_KEY_ID
-    postgres_password   = var.langfuse_postgres_password
-    clickhouse_password = var.langfuse_clickhouse_password
-    redis_password      = var.langfuse_redis_password
-  })
-}
+#   langfuse_helm_values = templatefile("${path.module}/templates/langfuse_helm_values.yaml.tpl", {
+#     hostname            = "langfuse.apps.openshift-01.hashicorp.local"
+#     storage_class       = "ocs-storagecluster-cephfs"
+#     s3_endpoint         = "http://rook-ceph-rgw-ocs-storagecluster-cephobjectstore.openshift-storage.svc"
+#     s3_bucket           = data.kubernetes_resource.langfuse_s3.object.spec.bucketName
+#     s3_access_key       = data.kubernetes_secret.langfuse_s3.data.AWS_ACCESS_KEY_ID
+#     postgres_password   = var.langfuse_postgres_password
+#     clickhouse_password = var.langfuse_clickhouse_password
+#     redis_password      = var.langfuse_redis_password
+#   })
+# }
 
-# Namespace
-resource "kubernetes_namespace" "langfuse" {
-  metadata {
-    name = "langfuse"
-  }
+# # Namespace
+# resource "kubernetes_namespace" "langfuse" {
+#   metadata {
+#     name = "langfuse"
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      metadata.0.annotations["openshift.io/sa.scc.mcs"],
-      metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
-      metadata.0.annotations["openshift.io/sa.scc.uid-range"],
-      metadata.0.labels
+#   lifecycle {
+#     ignore_changes = [
+#       metadata.0.annotations["openshift.io/sa.scc.mcs"],
+#       metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
+#       metadata.0.annotations["openshift.io/sa.scc.uid-range"],
+#       metadata.0.labels
 
-    ]
-  }
-}
+#     ]
+#   }
+# }
 
-# S3 Bucket Claim for Langfuse blob storage
-resource "kubernetes_manifest" "langfuse_s3_bucket" {
-  depends_on = [kubernetes_namespace.langfuse]
-  manifest = {
-    apiVersion = "objectbucket.io/v1alpha1"
-    kind       = "ObjectBucketClaim"
-    metadata = {
-      name      = "langfuse"
-      namespace = "langfuse"
-    }
-    spec = {
-      generateBucketName = "langfuse"
-      storageClassName   = "ocs-storagecluster-ceph-rgw"
-    }
-  }
-}
-
-# Data source for S3 credentials (created by ObjectBucketClaim)
-data "kubernetes_secret" "langfuse_s3" {
-  depends_on = [kubernetes_manifest.langfuse_s3_bucket]
-  metadata {
-    name      = "langfuse"
-    namespace = "langfuse"
-  }
-}
-
-# Data source for S3 bucket info
-data "kubernetes_resource" "langfuse_s3" {
-  depends_on  = [kubernetes_manifest.langfuse_s3_bucket]
-  api_version = "objectbucket.io/v1alpha1"
-  kind        = "ObjectBucketClaim"
-  metadata {
-    name      = "langfuse"
-    namespace = "langfuse"
-  }
-}
-
-# Secrets for Langfuse application
-resource "kubernetes_secret" "langfuse_secrets" {
-  depends_on = [kubernetes_namespace.langfuse]
-  metadata {
-    name      = "langfuse-secrets"
-    namespace = "langfuse"
-  }
-
-  data = {
-    NEXTAUTH_SECRET         = var.langfuse_nextauth_secret
-    SALT                    = var.langfuse_salt
-    LANGFUSE_ENCRYPTION_KEY = var.langfuse_encryption_key
-  }
-}
-
-# Helm release for Langfuse
-resource "helm_release" "langfuse" {
-  depends_on = [
-    kubernetes_namespace.langfuse,
-    kubernetes_secret.langfuse_secrets,
-    kubernetes_manifest.langfuse_s3_bucket,
-    data.kubernetes_secret.langfuse_s3,
-    data.kubernetes_resource.langfuse_s3
-  ]
-
-  name             = "langfuse"
-  repository       = "https://langfuse.github.io/langfuse-k8s"
-  chart            = "langfuse"
-  namespace        = "langfuse"
-  create_namespace = false
-  wait             = false
-  force_update     = true
-  timeout          = 600
-
-  values = [local.langfuse_helm_values]
-}
-
-# OpenShift Route for external access
-resource "kubernetes_manifest" "langfuse_route" {
-  depends_on = [helm_release.langfuse]
-  manifest   = provider::kubernetes::manifest_decode(local.langfuse_route)
-}
-
-# Vault integration resources (for future use)
-# Uncomment these when ready to integrate with Vault Secrets Operator
-
-# resource "kubernetes_manifest" "langfuse_vault_auth_sa" {
+# # S3 Bucket Claim for Langfuse blob storage
+# resource "kubernetes_manifest" "langfuse_s3_bucket" {
 #   depends_on = [kubernetes_namespace.langfuse]
-#   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_auth_sa)
+#   manifest = {
+#     apiVersion = "objectbucket.io/v1alpha1"
+#     kind       = "ObjectBucketClaim"
+#     metadata = {
+#       name      = "langfuse"
+#       namespace = "langfuse"
+#     }
+#     spec = {
+#       generateBucketName = "langfuse"
+#       storageClassName   = "ocs-storagecluster-ceph-rgw"
+#     }
+#   }
 # }
 
-# resource "kubernetes_manifest" "langfuse_vault_connection" {
+# # Data source for S3 credentials (created by ObjectBucketClaim)
+# data "kubernetes_secret" "langfuse_s3" {
+#   depends_on = [kubernetes_manifest.langfuse_s3_bucket]
+#   metadata {
+#     name      = "langfuse"
+#     namespace = "langfuse"
+#   }
+# }
+
+# # Data source for S3 bucket info
+# data "kubernetes_resource" "langfuse_s3" {
+#   depends_on  = [kubernetes_manifest.langfuse_s3_bucket]
+#   api_version = "objectbucket.io/v1alpha1"
+#   kind        = "ObjectBucketClaim"
+#   metadata {
+#     name      = "langfuse"
+#     namespace = "langfuse"
+#   }
+# }
+
+# # Secrets for Langfuse application
+# resource "kubernetes_secret" "langfuse_secrets" {
 #   depends_on = [kubernetes_namespace.langfuse]
-#   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_connection)
+#   metadata {
+#     name      = "langfuse-secrets"
+#     namespace = "langfuse"
+#   }
+
+#   data = {
+#     NEXTAUTH_SECRET         = var.langfuse_nextauth_secret
+#     SALT                    = var.langfuse_salt
+#     LANGFUSE_ENCRYPTION_KEY = var.langfuse_encryption_key
+#   }
 # }
 
-# resource "kubernetes_manifest" "langfuse_vault_auth" {
-#   depends_on = [kubernetes_manifest.langfuse_vault_connection]
-#   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_auth)
+# # Helm release for Langfuse
+# resource "helm_release" "langfuse" {
+#   depends_on = [
+#     kubernetes_namespace.langfuse,
+#     kubernetes_secret.langfuse_secrets,
+#     kubernetes_manifest.langfuse_s3_bucket,
+#     data.kubernetes_secret.langfuse_s3,
+#     data.kubernetes_resource.langfuse_s3
+#   ]
+
+#   name             = "langfuse"
+#   repository       = "https://langfuse.github.io/langfuse-k8s"
+#   chart            = "langfuse"
+#   namespace        = "langfuse"
+#   create_namespace = false
+#   wait             = false
+#   force_update     = true
+#   timeout          = 600
+
+#   values = [local.langfuse_helm_values]
 # }
 
-# resource "kubernetes_manifest" "langfuse_pki_cert" {
-#   depends_on = [kubernetes_manifest.langfuse_vault_auth]
-#   manifest   = provider::kubernetes::manifest_decode(local.langfuse_pki_cert)
+# # OpenShift Route for external access
+# resource "kubernetes_manifest" "langfuse_route" {
+#   depends_on = [helm_release.langfuse]
+#   manifest   = provider::kubernetes::manifest_decode(local.langfuse_route)
 # }
+
+# # Vault integration resources (for future use)
+# # Uncomment these when ready to integrate with Vault Secrets Operator
+
+# # resource "kubernetes_manifest" "langfuse_vault_auth_sa" {
+# #   depends_on = [kubernetes_namespace.langfuse]
+# #   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_auth_sa)
+# # }
+
+# # resource "kubernetes_manifest" "langfuse_vault_connection" {
+# #   depends_on = [kubernetes_namespace.langfuse]
+# #   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_connection)
+# # }
+
+# # resource "kubernetes_manifest" "langfuse_vault_auth" {
+# #   depends_on = [kubernetes_manifest.langfuse_vault_connection]
+# #   manifest   = provider::kubernetes::manifest_decode(local.langfuse_vault_auth)
+# # }
+
+# # resource "kubernetes_manifest" "langfuse_pki_cert" {
+# #   depends_on = [kubernetes_manifest.langfuse_vault_auth]
+# #   manifest   = provider::kubernetes::manifest_decode(local.langfuse_pki_cert)
+# # }
--- sentry.tf
+++ sentry.tf
@@ -1,174 +1,174 @@
-# Sentry - Self-hosted Error Tracking Platform
-# Deployed via Helm chart with bundled PostgreSQL and S3 storage (Ceph RGW)
-# Reference: https://github.com/sentry-kubernetes/charts
+# # Sentry - Self-hosted Error Tracking Platform
+# # Deployed via Helm chart with bundled PostgreSQL and S3 storage (Ceph RGW)
+# # Reference: https://github.com/sentry-kubernetes/charts
 
-locals {
-  sentry_route = file("${path.module}/manifests/sentry/sentry-route.yaml")
+# locals {
+#   sentry_route = file("${path.module}/manifests/sentry/sentry-route.yaml")
 
-  sentry_helm_values = templatefile("${path.module}/templates/sentry_helm_values.yaml.tpl", {
-    hostname          = "sentry.apps.openshift-01.hashicorp.local"
-    storage_class     = "ocs-storagecluster-cephfs"
-    secret_key        = var.sentry_secret_key
-    admin_email       = var.sentry_admin_email
-    admin_password    = var.sentry_admin_password
-    postgres_password = var.sentry_postgres_password
-    redis_password    = var.sentry_redis_password
-    s3_endpoint       = "http://rook-ceph-rgw-ocs-storagecluster-cephobjectstore.openshift-storage.svc"
-    s3_bucket         = data.kubernetes_resource.sentry_s3.object.spec.bucketName
-    s3_access_key     = data.kubernetes_secret.sentry_s3.data.AWS_ACCESS_KEY_ID
-    s3_secret_key     = data.kubernetes_secret.sentry_s3.data.AWS_SECRET_ACCESS_KEY
-    mail_host         = var.sentry_mail_host
-    mail_port         = var.sentry_mail_port
-    mail_username     = var.sentry_mail_username
-    mail_password     = var.sentry_mail_password
-  })
-}
+#   sentry_helm_values = templatefile("${path.module}/templates/sentry_helm_values.yaml.tpl", {
+#     hostname          = "sentry.apps.openshift-01.hashicorp.local"
+#     storage_class     = "ocs-storagecluster-cephfs"
+#     secret_key        = var.sentry_secret_key
+#     admin_email       = var.sentry_admin_email
+#     admin_password    = var.sentry_admin_password
+#     postgres_password = var.sentry_postgres_password
+#     redis_password    = var.sentry_redis_password
+#     s3_endpoint       = "http://rook-ceph-rgw-ocs-storagecluster-cephobjectstore.openshift-storage.svc"
+#     s3_bucket         = data.kubernetes_resource.sentry_s3.object.spec.bucketName
+#     s3_access_key     = data.kubernetes_secret.sentry_s3.data.AWS_ACCESS_KEY_ID
+#     s3_secret_key     = data.kubernetes_secret.sentry_s3.data.AWS_SECRET_ACCESS_KEY
+#     mail_host         = var.sentry_mail_host
+#     mail_port         = var.sentry_mail_port
+#     mail_username     = var.sentry_mail_username
+#     mail_password     = var.sentry_mail_password
+#   })
+# }
 
-# Namespace
-resource "kubernetes_namespace" "sentry" {
-  metadata {
-    name = "sentry"
-  }
+# # Namespace
+# resource "kubernetes_namespace" "sentry" {
+#   metadata {
+#     name = "sentry"
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      metadata.0.annotations["openshift.io/sa.scc.mcs"],
-      metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
-      metadata.0.annotations["openshift.io/sa.scc.uid-range"],
-      metadata.0.labels
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       metadata.0.annotations["openshift.io/sa.scc.mcs"],
+#       metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
+#       metadata.0.annotations["openshift.io/sa.scc.uid-range"],
+#       metadata.0.labels
+#     ]
+#   }
+# }
 
-# S3 ObjectBucketClaim for Sentry blob storage
-resource "kubernetes_manifest" "sentry_s3_bucket" {
-  depends_on = [kubernetes_namespace.sentry]
-  manifest = {
-    apiVersion = "objectbucket.io/v1alpha1"
-    kind       = "ObjectBucketClaim"
-    metadata = {
-      name      = "sentry"
-      namespace = "sentry"
-    }
-    spec = {
-      generateBucketName = "sentry"
-      storageClassName   = "ocs-storagecluster-ceph-rgw"
-    }
-  }
-}
-
-# Data source for S3 credentials (created by ObjectBucketClaim)
-data "kubernetes_secret" "sentry_s3" {
-  depends_on = [kubernetes_manifest.sentry_s3_bucket]
-  metadata {
-    name      = "sentry"
-    namespace = "sentry"
-  }
-}
-
-# Data source for S3 bucket info
-data "kubernetes_resource" "sentry_s3" {
-  depends_on  = [kubernetes_manifest.sentry_s3_bucket]
-  api_version = "objectbucket.io/v1alpha1"
-  kind        = "ObjectBucketClaim"
-  metadata {
-    name      = "sentry"
-    namespace = "sentry"
-  }
-}
-
-# Secrets for Sentry application
-resource "kubernetes_secret" "sentry_secrets" {
-  depends_on = [kubernetes_namespace.sentry]
-  metadata {
-    name      = "sentry-secrets"
-    namespace = "sentry"
-  }
-
-  data = {
-    SENTRY_SECRET_KEY = var.sentry_secret_key
-  }
-}
-
-# OpenShift SCC - Grant anyuid to SAs for stateful services
-# ClickHouse, PostgreSQL, Redis, Kafka need to run as specific UIDs
-# Service account names discovered via: kubectl get sa -n sentry
-resource "kubernetes_manifest" "sentry_anyuid_scc" {
-  depends_on = [kubernetes_namespace.sentry]
-  manifest = {
-    apiVersion = "rbac.authorization.k8s.io/v1"
-    kind       = "ClusterRoleBinding"
-    metadata = {
-      name = "sentry-anyuid-scc"
-    }
-    roleRef = {
-      apiGroup = "rbac.authorization.k8s.io"
-      kind     = "ClusterRole"
-      name     = "system:openshift:scc:anyuid"
-    }
-    subjects = [
-      {
-        kind      = "ServiceAccount"
-        name      = "default"
-        namespace = "sentry"
-      },
-      {
-        kind      = "ServiceAccount"
-        name      = "sentry-sentry-redis"
-        namespace = "sentry"
-      },
-      {
-        kind      = "ServiceAccount"
-        name      = "sentry-kafka"
-        namespace = "sentry"
-      }
-    ]
-  }
-}
-
-# Helm release for Sentry
-resource "helm_release" "sentry" {
-  depends_on = [
-    kubernetes_namespace.sentry,
-    kubernetes_manifest.sentry_s3_bucket,
-    kubernetes_secret.sentry_secrets,
-    kubernetes_manifest.sentry_anyuid_scc,
-    data.kubernetes_secret.sentry_s3,
-    data.kubernetes_resource.sentry_s3
-  ]
-
-  name             = "sentry"
-  repository       = "https://sentry-kubernetes.github.io/charts"
-  chart            = "sentry"
-  namespace        = "sentry"
-  create_namespace = false
-  wait             = false
-  force_update     = true
-  timeout          = 1200 # Sentry takes longer to deploy
-
-  values = [local.sentry_helm_values]
-}
-
-# OpenShift Route for external access
-resource "kubernetes_manifest" "sentry_route" {
-  depends_on = [helm_release.sentry]
-  manifest   = provider::kubernetes::manifest_decode(local.sentry_route)
-}
-
-# Optional: Vault integration resources (for future use)
-# Uncomment these when ready to integrate with Vault Secrets Operator
-
-# resource "kubernetes_manifest" "sentry_vault_auth_sa" {
+# # S3 ObjectBucketClaim for Sentry blob storage
+# resource "kubernetes_manifest" "sentry_s3_bucket" {
 #   depends_on = [kubernetes_namespace.sentry]
-#   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_auth_sa)
+#   manifest = {
+#     apiVersion = "objectbucket.io/v1alpha1"
+#     kind       = "ObjectBucketClaim"
+#     metadata = {
+#       name      = "sentry"
+#       namespace = "sentry"
+#     }
+#     spec = {
+#       generateBucketName = "sentry"
+#       storageClassName   = "ocs-storagecluster-ceph-rgw"
+#     }
+#   }
 # }
 
-# resource "kubernetes_manifest" "sentry_vault_connection" {
+# # Data source for S3 credentials (created by ObjectBucketClaim)
+# data "kubernetes_secret" "sentry_s3" {
+#   depends_on = [kubernetes_manifest.sentry_s3_bucket]
+#   metadata {
+#     name      = "sentry"
+#     namespace = "sentry"
+#   }
+# }
+
+# # Data source for S3 bucket info
+# data "kubernetes_resource" "sentry_s3" {
+#   depends_on  = [kubernetes_manifest.sentry_s3_bucket]
+#   api_version = "objectbucket.io/v1alpha1"
+#   kind        = "ObjectBucketClaim"
+#   metadata {
+#     name      = "sentry"
+#     namespace = "sentry"
+#   }
+# }
+
+# # Secrets for Sentry application
+# resource "kubernetes_secret" "sentry_secrets" {
 #   depends_on = [kubernetes_namespace.sentry]
-#   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_connection)
+#   metadata {
+#     name      = "sentry-secrets"
+#     namespace = "sentry"
+#   }
+
+#   data = {
+#     SENTRY_SECRET_KEY = var.sentry_secret_key
+#   }
 # }
 
-# resource "kubernetes_manifest" "sentry_vault_auth" {
-#   depends_on = [kubernetes_manifest.sentry_vault_connection]
-#   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_auth)
+# # OpenShift SCC - Grant anyuid to SAs for stateful services
+# # ClickHouse, PostgreSQL, Redis, Kafka need to run as specific UIDs
+# # Service account names discovered via: kubectl get sa -n sentry
+# resource "kubernetes_manifest" "sentry_anyuid_scc" {
+#   depends_on = [kubernetes_namespace.sentry]
+#   manifest = {
+#     apiVersion = "rbac.authorization.k8s.io/v1"
+#     kind       = "ClusterRoleBinding"
+#     metadata = {
+#       name = "sentry-anyuid-scc"
+#     }
+#     roleRef = {
+#       apiGroup = "rbac.authorization.k8s.io"
+#       kind     = "ClusterRole"
+#       name     = "system:openshift:scc:anyuid"
+#     }
+#     subjects = [
+#       {
+#         kind      = "ServiceAccount"
+#         name      = "default"
+#         namespace = "sentry"
+#       },
+#       {
+#         kind      = "ServiceAccount"
+#         name      = "sentry-sentry-redis"
+#         namespace = "sentry"
+#       },
+#       {
+#         kind      = "ServiceAccount"
+#         name      = "sentry-kafka"
+#         namespace = "sentry"
+#       }
+#     ]
+#   }
 # }
+
+# # Helm release for Sentry
+# resource "helm_release" "sentry" {
+#   depends_on = [
+#     kubernetes_namespace.sentry,
+#     kubernetes_manifest.sentry_s3_bucket,
+#     kubernetes_secret.sentry_secrets,
+#     kubernetes_manifest.sentry_anyuid_scc,
+#     data.kubernetes_secret.sentry_s3,
+#     data.kubernetes_resource.sentry_s3
+#   ]
+
+#   name             = "sentry"
+#   repository       = "https://sentry-kubernetes.github.io/charts"
+#   chart            = "sentry"
+#   namespace        = "sentry"
+#   create_namespace = false
+#   wait             = false
+#   force_update     = true
+#   timeout          = 1200 # Sentry takes longer to deploy
+
+#   values = [local.sentry_helm_values]
+# }
+
+# # OpenShift Route for external access
+# resource "kubernetes_manifest" "sentry_route" {
+#   depends_on = [helm_release.sentry]
+#   manifest   = provider::kubernetes::manifest_decode(local.sentry_route)
+# }
+
+# # Optional: Vault integration resources (for future use)
+# # Uncomment these when ready to integrate with Vault Secrets Operator
+
+# # resource "kubernetes_manifest" "sentry_vault_auth_sa" {
+# #   depends_on = [kubernetes_namespace.sentry]
+# #   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_auth_sa)
+# # }
+
+# # resource "kubernetes_manifest" "sentry_vault_connection" {
+# #   depends_on = [kubernetes_namespace.sentry]
+# #   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_connection)
+# # }
+
+# # resource "kubernetes_manifest" "sentry_vault_auth" {
+# #   depends_on = [kubernetes_manifest.sentry_vault_connection]
+# #   manifest   = provider::kubernetes::manifest_decode(local.sentry_vault_auth)
+# # }
